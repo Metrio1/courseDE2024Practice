1373d723026bf6b5974f08943238dde776f381b2
Mettle <metrio@inbox.ru>
2024-12-03 16:19:55 +0500

    ConfirmModal stylized

--- src/shared/lib/utils/getGeneratedAttrs.js
+++ src/shared/lib/utils/getGeneratedAttrs.js
@@ -1,7 +1,18 @@
 /**
  * Генерируем атрибуты для компонентов
+ * @param {Array} attributes - Массив атрибутов в формате [{ name, value } или строка]
  * @return {String}
  */
 export const getGeneratedAttrs = (attributes = []) => {
-  return attributes.map((attr) => `${attr.name}=${attr.value}`).join(" ");
+  return attributes
+      .map((attr) => {
+        if (typeof attr === "string") {
+          return attr;
+        } else if (typeof attr === "object" && attr.name) {
+          return attr.value ? `${attr.name}="${attr.value}"` : `${attr.name}`;
+        }
+        return "";
+      })
+      .filter(Boolean)
+      .join(" ");
 };
--- src/shared/ui/ConfirmModal/ui/ConfirmModal.js
+++ src/shared/ui/ConfirmModal/ui/ConfirmModal.js
@@ -1,14 +1,14 @@
+import {Button} from "#shared/ui/Button/index.js";
+import {CancelIcon, CheckIcon} from "#shared/ui/Icons/index.js";
+
+
 export const getConfirmModalTemplate = (message = "Вы уверены?") => {
     return `
         <div class="confirm-modal">
             <p class="confirm-modal__message">${message}</p>
             <div class="confirm-modal__buttons">
-                <button data-js-confirm-btn class="confirm-modal__button confirm-modal__button--confirm">
-                    Да
-                </button>
-                <button data-js-cancel-btn class="confirm-modal__button confirm-modal__button--cancel">
-                    Нет
-                </button>
+                ${Button({ text: "Да", iconSlot: CheckIcon(), extraClasses: ["confirm-modal__button", "confirm-modal__button--confirm", "btn--isGreenLightIcon"], extraAttrs: ["data-js-confirm-btn"] })}
+                ${Button({ text: "Нет", iconSlot: CancelIcon(), extraClasses: ["btn--isRedIcon", "confirm-modal__button", "confirm-modal__button--cancel"], extraAttrs: ["data-js-cancel-btn"] })}    
             </div>
         </div>
     `;
